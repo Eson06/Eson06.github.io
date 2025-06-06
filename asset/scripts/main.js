564d36701bb5e188e9a541671d4ca6bf6e7ef08c
Eson06 <jasongarab@gmail.com>
2025-06-06 20:51:04 +0800

    initial commit

---
+++ asset/scripts/main.js
@@ -0,0 +1,41 @@
+// const firstNum = 300;
+// const secNum = 7;
+
+// const sum = firstNum + secNum;
+// console.log("Sum (using + operator):", sum);
+
+// function myFunc(x, y, operator) {
+//   switch (operator) {
+//     case '+':
+//       return x + y;
+//     case '-':
+//       return x - y;
+//     case '*':
+//       return x * y;
+//     case '/':
+//       return y !== 0 ? x / y : 'Cannot divide by zero';
+//     case '**':
+//       return x ** y;
+//     case '%':
+//       return x % y;
+//     default:
+//       return 'Invalid operator';
+//   }
+// }
+
+// console.log("Sum (myFunc):", myFunc(firstNum, secNum, '+'));
+// console.log("Difference:", myFunc(firstNum, secNum, '-'));
+// console.log("Product:", myFunc(firstNum, secNum, '*'));
+// console.log("Quotient:", myFunc(firstNum, secNum, '/'));
+// console.log("Exponent:", myFunc(firstNum, secNum, '**'));
+// console.log("Modulo:", myFunc(firstNum, secNum, '%'));
+
+// for (let x = 0; x <= 100; x++) {
+//     if (x % 2 !== 0) {
+//       console.log(x);
+//     }
+//   }
+
+
+  
+  
